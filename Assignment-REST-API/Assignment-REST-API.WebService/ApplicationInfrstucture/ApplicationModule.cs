e407b61bba634ebb9564ec38cdba7d66b037135a
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-02-22 00:41:58 +0530

    Application Middleware Configurations And Database Configuration Implimented

---
+++ Assignment-REST-API/Assignment-REST-API.WebService/ApplicationInfrstucture/ApplicationModule.cs
@@ -0,0 +1,25 @@
+﻿using Assignment_REST_API.Business;
+using Assignment_REST_API.Business.Interface;
+using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_REST_API.WebService.ApplicationInfrstucture
+{
+    public class ApplicationModule : Autofac.Module
+    {
+        public ApplicationModule()
+        {
+
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SAPService>()
+                 .As<ISAPService>()
+                 .SingleInstance();
+        }
+    }
+}
--- Assignment-REST-API/Assignment-REST-API.WebService/Startup.cs
+++ Assignment-REST-API/Assignment-REST-API.WebService/Startup.cs
@@ -1,7 +1,12 @@
+using Assignment_REST_API.Data.Data;
+using Assignment_REST_API.WebService.ApplicationInfrstucture;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,14 +29,46 @@ namespace Assignment_REST_API.WebService
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("AppDbConn")));
+            //services.AddDbContext(Configuration);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Assignment_REST_API.WebService", Version = "v1" });
             });
+
+            var container = new ContainerBuilder();
+            container.Populate(services);
+
+            container.RegisterModule(new ApplicationModule());
+
+            return new AutofacServiceProvider(container.Build());
+        }
+
+        public static class CustomExtenstionMethods
+        {
+            //public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+            //{
+            //    var connectionString = configuration["ConnectionString"];
+
+            //    services.AddDbContext<ApplicationDbContext>(options =>
+            //    {
+
+            //        options.UseLazyLoadingProxies();
+            //        options.UseSqlServer(configuration["ConnectionString"],
+            //            sqlServerOptionsAction: sqlOptions =>
+            //            {
+            //                sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+            //            });
+            //    });
+
+            //    return services;
+                 
+           // }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
